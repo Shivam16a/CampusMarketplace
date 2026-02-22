a9eecd1e7a9f6995302f8946be986e5cd8b2d41a
Shivam16a <shivam123hjp@gmail.com>
2026-02-22 22:28:16 +0530

    creating User models and schema

---
+++ backend/routes/userModels.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: { type: Number, required: true },
+    gender: { type: String, required: true },
+    collagemane: { type: String, required: true },
+    department: { type: String, required: true },
+    role: {
+        type: String,
+        enum:["user","Seller"],
+        default:"user"
+    },
+    isAdmin:false,
+    password: {type:String,required:true},
+}, { timestamps: true });
+
+const User = new mongoose.model("User",userSchema);
+module.exports = User;
