41c51932eb8938ee5b593bcd2f2a0ab92a6b9dad
Shivam16a <shivam123hjp@gmail.com>
2026-02-25 09:50:34 +0530

    deleting everything

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,8 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
-import Home from './pages/Home';
-import Register from './pages/Register';
-import Login from './pages/Login'
-import Profile from './Components/Profile';
-import Users from './Admin/Users';
-import NotFound from './Components/NotFound';
-import Layout from './Components/Layout';
-
-import './App.css'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-
-        {/* Layout wrapper */}
-        <Route path="/" element={<Layout />}>
-
-          <Route index element={<Home />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="users" element={<Users />} />
-
-        </Route>
-
-        {/* Auth pages without sidebar */}
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-
-        <Route path="*" element={<NotFound />} />
-
-      </Routes>
-    </BrowserRouter>
+    <div>App</div>
   )
 }
 
