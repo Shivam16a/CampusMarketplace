398d47bb40b31642d3801a34c5f41afe695d94bc
Shivam16a <shivam123hjp@gmail.com>
2026-03-15 11:16:17 +0530

    adding ratelimite on login and registration

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const router = express.Router();
 
 const {
@@ -9,14 +10,22 @@ const {
     deleteProfile,
     adminUpdateUser,
     adminDeleteUser,
-    getAllUsers  
+    getAllUsers
 } = require('../controllers/userController.js');
+
+
+// login/register limiter (strict)
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 min
+    max: 5, // sirf 5 attempts
+    message: 'Too many login/register attempts. Try again after 15 minutes.'
+});
 
 const { protect, adminOnly } = require('../middleware/authMiddleware.js');
 const upload = require('../middleware/uploadMiddleware.js');
 
-router.post('/register', upload.single('profilepic'), registerUser);
-router.post('/login', loginUser);
+router.post('/register', authLimiter, upload.single('profilepic'), registerUser);
+router.post('/login', authLimiter, loginUser);
 router.get('/profile', protect, getProfile);
 
 // USER
@@ -24,7 +33,7 @@ router.put('/profile', protect, upload.single('profilepic'), updateProfile);
 router.delete('/profile', protect, deleteProfile);
 
 // ADMIN
-router.get('/all', protect, adminOnly, getAllUsers);   
+router.get('/all', protect, adminOnly, getAllUsers);
 router.put('/admin/:id', protect, adminOnly, upload.single('profilepic'), adminUpdateUser);
 router.delete('/admin/:id', protect, adminOnly, adminDeleteUser);
 
